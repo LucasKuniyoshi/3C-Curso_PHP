9ba33e853c13aa5d6ab128eacf102e856ada1967
Lucas Kuniyoshi <lucaskuniyoshi2002@gmail.com>
2024-08-28 20:07:19 -0300

    teste

---
+++ 2_conceitos_basicos/1_case_sensitive/index.php
@@ -0,0 +1,14 @@
+<?php
+    //Não é case sensitive
+    echo "teste";
+    eCho "testando 2 <br>";
+    ECHO "testando 3 <br>";
+
+    //É case sensitive
+    $nome = "Lucas";
+    $NOME = "Teste";
+
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
+?>
