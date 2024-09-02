3d5e5d79c2308fc81f2582c53cdbba03d86d5510
Lucas Kuniyoshi <lucaskuniyoshi2002@gmail.com>
2024-09-02 20:17:35 -0300

    update home local

---
+++ Exercicios/Ex3.php
@@ -0,0 +1,49 @@
+<?php
+    //Ex 1
+    // $a = 1;
+    // while($a <= 10){
+    //     echo $a . "<br>";
+    //     $a++;
+    // }
+
+    //Ex 2
+    // $a = 1;
+    // $soma = 0;
+    // while($a <= 10){
+    //     $soma += $a;
+    //     $a++;
+    // }
+    // $media = $soma/10;
+    // echo "Soma: $soma , Média: $media.";
+
+    //Ex 3
+    // $a = [12,3,4,5,6,7,8,9,10,23];
+    // $i = 0;
+    // $soma = 0;
+    // while($i < 10){
+    //     $soma += $a[$i];
+    //     $i++;
+    // }
+    // $media = $soma/count($a);
+    // echo "Soma: $soma , Média: $media , QtdeValores: " . count($a);
+
+    //Ex 4
+    // $a = 50;
+    // $soma = 0;
+    // $i = 0;
+    // while($a <= 70){
+    //     if($a % 2 == 0){
+    //         $soma += $a;
+    //         $i++;
+    //         echo "A: $a , Soma: $soma , I: $i <br>";
+    //     }
+    //     $a++;
+    // }
+    // $media = $soma/$i;
+    // echo "Soma: $soma , Média: $media.";
+
+    //Ex 5
+    $a = 2; //readline()
+    while($a != 0){
+
+    }
